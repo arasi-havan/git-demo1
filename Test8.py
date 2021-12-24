271653997f52dfdecae094c27e73c16a04c1f8a2
arasi-havan <94338570+arasi-havan@users.noreply.github.com>
2021-12-24 17:27:59 +1100

    Intial version

---
+++ Test8.py
@@ -0,0 +1,25 @@
+print("This is the sum of two numbers")
+a=2
+b=3
+c=a+b
+print(c)
+a=3
+b=4
+c=a+b
+print(c)
+# This is difference of two numbers
+l1=235
+l2=216
+l3=l1-l2
+print(l3)
+l1=1267
+l2=678
+l3=l1-l2
+print(l3)
+print("This is multiplication of two numbers")
+value1=564
+value2=234
+value3=value1*value2
+print("The product of two numbers", value3)
+print("The product of two numbers"+"djheymjymjyyy")
+print("The product of two numbers"+" $ "+"djheymjymjyyy")
